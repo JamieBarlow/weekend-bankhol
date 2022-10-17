3c16339d24aeca8c721b6a2074407ceeccb7a5fa
Jamie Barlow <jamiebarlow@gmail.com>
2022-10-17 08:32:33 +0100

    Add function to display claim dates on weekends as next working day

--- public/js/functions.js
+++ public/js/functions.js
@@ -24,51 +24,59 @@ function getBankHols() {
         });
 }
 
+const displayBankHols = (thisYear) => {
+    const header = document.createElement('h2');
+    header.innerText = `Bank holiday dates for ${year}`;
+    const list = document.createElement('ul');
+    results.append(header, list);
+    for (let date of thisYear) {
+        const listItem = document.createElement('li');
+        let ukDate = convertDateToUK(date);
+        // nonProcessingDays.push(ukDate);
+        // console.log(`Bank hol date: ${date}`);
+        nonProcessingDays.push(convertUSDateToObject(date));
+        // console.log(nonProcessingDays);
+        listItem.innerText = ukDate;
+        list.append(listItem);
+    }
+    // console.log(`Display bank hols: ${nonProcessingDays[3]}`)
+}
+
 const convertDateToUK = (date) => {
     // console.log(date);
     date = `${date.slice(8, 10)}/${date.slice(5, 7)}/${date.slice(0, 4)}`;
     return date;
 }
 
-const displayBankHols = (thisYear) => {
-    const header = document.createElement('h2');
-    header.innerText = `Bank holiday dates for ${year}`;
-    const list = document.createElement('ul');
-    results.append(header, list);
-    for (date of thisYear) {
-        const listItem = document.createElement('li');
-        let ukDate = convertDateToUK(date);
-        // nonProcessingDays.push(ukDate);
-        // console.log(`Bank hol date: ${date}`);
-        nonProcessingDays.push(convertDateToObject(date));
-        // console.log(nonProcessingDays);
-        listItem.innerText = ukDate;
-        list.append(listItem);
-    }
-}
-
-const convertDateToObject = (date) => {
-    const dateObject = new Date(date);
+const convertUSDateToObject = (date) => {
+    let dateObject = new Date(date);
     // console.log(`date object: ${dateObject}`)
     return dateObject;
 }
 
+const convertUKDateToObject = (date) => {
+    let dateObject = new Date(`${date.slice(3, 5)}/${date.slice(0, 2)}/${date.slice(6, 10)}`);
+    return dateObject;
+}
+
 const getWeekends = (year) => {
-    let endOfYear = new Date(`December 31, ${year}`);
     let daysOfYear = [];
     // get all days of year
-    for (let i= new Date(`January 1, ${year}`); i <= endOfYear; i.setDate(i.getDate() + 1)) {
-        daysOfYear.push(new Date(i));
+    for (let i=0; i < 365; i++) {
+        let date = new Date(`January 1, ${year}`);
+        date.setDate(date.getDate() + i);
+        daysOfYear.push(date);
     }
     // get weekends only by removing week days
     let weekends = [];
-    for (day of daysOfYear) {
+    for (let day of daysOfYear) {
         if (day.getDay() === 6 || day.getDay() === 0) {
-            weekends.push(day.toLocaleDateString('en-GB'));
             nonProcessingDays.push(day);
+            weekends.push(day.toLocaleDateString('en-GB'));
         }
     }
-    console.log(nonProcessingDays);
+    // console.log(`Getweekends: ${nonProcessingDays[3]}`)
+    // console.log(nonProcessingDays);
     displayWeekends(weekends);
 }
 
@@ -78,11 +86,13 @@ const displayWeekends = (weekends) => {
     header.innerText = `Weekend dates for ${year}`;
     const list = document.createElement('ul');
     results.append(header, list);
-    for (date of weekends) {
+    for (let date of weekends) {
         const listItem = document.createElement('li');
         listItem.innerText = date;
         list.append(listItem);
     }
+    // console.log(`Display weekends: ${nonProcessingDays[3]}`)
+    displayProcessingDays();
 }
 
 const displayProcessingDays = () => {
@@ -115,22 +125,39 @@ const displayProcessingDays = () => {
     table.rows[1].cells[7].innerText = "Step 6 - 2 working days after the payment date";
     // Column A dates
     let month = 1;
+    let claimDates = [];
     for (let i = 2; i < table.rows.length; i++) {
         if (i % 2 === 0) {
-            table.rows[i].cells[0].innerText = `05/${month.toLocaleString('en-US', {minimumIntegerDigits: 2})}/${year}`;
+            let claimDate = `05/${month.toLocaleString('en-US', {minimumIntegerDigits: 2})}/${year}`;
+            table.rows[i].cells[0].innerText = claimDate;
+            claimDates.push(convertUKDateToObject(claimDate));
+            
         } else if (i % 2 === 1) {
-            table.rows[i].cells[0].innerText = `19/${month.toLocaleString('en-US', {minimumIntegerDigits: 2})}/${year}`;
+            let claimDate = `19/${month.toLocaleString('en-US', {minimumIntegerDigits: 2})}/${year}`;
+            table.rows[i].cells[0].innerText = claimDate;
+            claimDates.push(convertUKDateToObject(claimDate));
             month++;
         }
     }
     // Column F dates
-    for (let i = 2; i < table.rows.length; i++) {
-        if (table.rows[i].cells[0].innerText === nonProcessingDays) {
-            console.log("match!")
+    for (let i = 0; i < claimDates.length; i++) {
+        for (let j = 0; j < nonProcessingDays.length; j++) {
+            if (claimDates[i].getTime() === nonProcessingDays[j].getTime()) {
+                // console.log(`Match!`)
+                let newDate = new Date(`${claimDates[i]}`);
+                newDate.setDate(newDate.getDate() + 1);
+                for (let k = 0; k < nonProcessingDays.length; k++) {
+                    if (newDate.getTime() === nonProcessingDays[k].getTime()) {
+                        newDate.setDate(newDate.getDate() + 1);
+                    }
+                }
+                table.rows[i + 2].cells[5].innerText = newDate;
+            }
         }
     }
-    console.log(nonProcessingDays);
-    console.log(nonProcessingDays[0]);
+    console.log(`Claim dates: ${claimDates}`);
+    console.log(`Non processing days: ${nonProcessingDays}`);
+    console.log(`Display processing days: ${nonProcessingDays[3]}`)
     processingDays.append(header, table);
 }
 
@@ -143,6 +170,5 @@ year.addEventListener('change', function() {
 button.addEventListener('submit', function(e) {
     e.preventDefault();
     getBankHols();
-    displayProcessingDays();
 })
 
